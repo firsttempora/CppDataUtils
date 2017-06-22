90db36e4d6182e77acb9c1cb415a5c0c81202cd5
Josh Laughner <joshlaugh5@yahoo.com>
2017-06-22 17:22:58 -0400

    It's still not working, but I suspect its because of a weird thing with template fxns
    I think the compiler is trying to create a template function for a
    case that will never happen, where it tries to make my boost internal
    function for the case where just a double (not a boost array) is passed
    because it doesn't know that in that case dims == 1 and so the internal
    is never called.

--- matrix_utils.cpp
+++ matrix_utils.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
+#include <limits>
 #include <boost/multi_array.hpp>
-
+#include <math.h>
+#include <vector>
 
 template<class T>
+void boost_minmax_internal(const T& arr, double* curr_min, std::vector<int>* ind_arr, size_t curr_dim){
+    typename T::size_type dims = arr.num_dimensions();
+    if( dims > 1 ){
+        for(typename T::index i=0; i<arr.shape()[0]; i++){
+            double last_min = *curr_min;
+            //T sub_arr = arr[0];
+            boost_minmax_internal(arr[0], curr_min, ind_arr, curr_dim+1);
+            if( *curr_min < last_min ){
+                (*ind_arr)[curr_dim] = i;
+            }
+        }
+    }else{
+        for(typename T::index i=0; i<arr.shape()[0]; i++){
+            if(arr[i] < *curr_min){
+                *curr_min = arr[i];
+                (*ind_arr)[curr_dim] = i;
+            }
+        }
+    }
+    return;
+}
+
+template<class T>
+std::vector<int> boost_min_inds(T& arr){
+    std::vector<int> inds(arr.num_dimensions());
+    double curr_min = std::numeric_limits<double>::max();
+    boost_minmax_internal(arr, &curr_min, &inds, (size_t)0);
+    return inds;
+}
+
+
+//main for testing only
+int main(){
+    typedef boost::multi_array<double,2> array;
+    array A(boost::extents[9][9]);
+    for(array::index i=0; i<A.shape()[0]; i++){
+        std::cout << "[ ";
+        for(array::index j=0; j<A.shape()[1]; j++){
+            double val = pow((double)i-5,2) + pow((double)j-3,2);
+//            std::cout << "(" << i << "," << j << ") =" << val << std::endl;
+            std::cout << val << " ";
+            A[i][j] = val;
+        }
+        std::cout << "]" << std::endl;
+    }
+
+    std::vector<int> min_inds = boost_min_inds(A);
+    std::cout << "Indicies of minimum value: ";
+    for(size_t i=0; i<A.num_dimensions(); i++){
+        std::cout << min_inds[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    return 0;
+}
+
+
+/*template<class T>
 int[] find_min_inds(T& arr){
     double min_val = 1.79E+308;
     return find_min_inds(arr, min_val);
@@ -36,4 +96,4 @@ int[] find_min_inds(T& arr, double* min_val){
     }
 
     return inds
-}
+}*/
